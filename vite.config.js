eb4f6200762aadd8fce5e21682c8fe842892dd67
fengchuan <fengchuanhn@gmail.com>
2022-10-08 23:51:16 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -45,7 +45,7 @@ export default defineConfig({
     host:true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:80',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
